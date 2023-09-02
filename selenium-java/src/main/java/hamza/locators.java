e4d75af8f70109d2b47752f9eba24c54435d40c6
Hamza Najeeb <hamxa.niaxi@gmail.com>
2023-09-02 19:59:05 +0300

    working with basic locators id, name and className

---
+++ selenium-java/src/main/java/hamza/locators.java
@@ -0,0 +1,28 @@
+package hamza;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class locators {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver",
+                "C:/Users/Hamza/chromedriver/chromedriver-win64/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.rahulshettyacademy.com/locatorspractice/");
+
+        // Now finding element by it's id
+        // .SendKeys() function will send some data as input
+        driver.findElement(By.id("inputUsername")).sendKeys("rahul");
+
+        // Finding element by it's name and adding input
+        driver.findElement(By.name("inputPassword")).sendKeys("hello123");
+
+        // Finding element by it's class and sending input through click() which will
+        // click the button
+        driver.findElement(By.className("signInBtn")).click();
+
+        driver.close();
+    }
+}
