d16ef67b0fea129793382a9434bbf0ba02e4df5c
Hamza Najeeb <hamxa.niaxi@gmail.com>
2023-12-28 16:09:47 +0200

    handled dropdowns where we have increments

---
+++ selenium-java/src/main/java/hamza/updatedStaticDropdown.java
@@ -0,0 +1,37 @@
+package hamza;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class updatedStaticDropdown {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver",
+                "C:/Users/Hamza/chromedriver/chromedriver-win64/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise");
+
+        // Selecting a dropdown tag
+        driver.findElement(By.id("divpaxinfo")).click();
+
+        // Adding sleep because the dropdown can take time to load
+        Thread.sleep(1000);
+
+        // Selecting the increase button to add one more passenger in our search. only
+        // selects once
+        driver.findElement(By.id("hrefIncAdt")).click();
+
+        // looping to select multiple times
+        for (int i = 0; i < 4; i++) {
+            driver.findElement(By.id("hrefIncAdt")).click();
+        }
+
+        String passengerText = driver.findElement(By.id("divpaxinfo")).getText();
+        System.out.println("Number of Passengers: " + passengerText);
+
+        Thread.sleep(1000);
+        driver.quit();
+
+    }
+}
