bc3378ddc0f4437c9925f157ea70736813cf059a
Hamza Najeeb <hamxa.niaxi@gmail.com>
2024-04-24 01:42:59 +0300

    removed edge driver import

--- selenium-java/src/main/java/hamza/seleniumIntroduction.java
+++ selenium-java/src/main/java/hamza/seleniumIntroduction.java
@@ -2,7 +2,6 @@ package hamza;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class seleniumIntroduction {
